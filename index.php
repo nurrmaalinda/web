68df469da7fb8bf1efeefc9cb6673051632a83bf
linda-hub <nurmalinda002@gmail.com>
2024-03-13 12:02:42 +0800

    ubah index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Linda</title>
+</head>
+<body>
+    <h1>Nurmalinda</h1>
+    <h2>E020322110</h2>
+</body>
+</html>
